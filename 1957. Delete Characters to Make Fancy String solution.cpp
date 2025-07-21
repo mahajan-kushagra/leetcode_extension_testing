f68504c302fc8b6c444aa78da890db2471ff0dbb
Kushagra Mahajan <126378378+mahajan-kushagra@users.noreply.github.com>
2025-07-22 00:37:21 +0530

    Optimize string filtering to remove consecutive duplicates

--- 1957. Delete Characters to Make Fancy String solution.cpp
+++ 1957. Delete Characters to Make Fancy String solution.cpp
@@ -15,23 +15,4 @@ public:
         }
         return result;
     }
-    /* string makeFancyS tring2(string s) {
-        int offset = 0;
-        bool same01, same12;
-        for (int i = 2; i < s.size() - offset;) {
-            s[i] = s[i + offset];
-            same01 = (s[i - 1] == s[i - 2]);
-            same12 = (s[i - 1] == s[i]);
-            if (same01 && same12) {
-                while (i + offset < s.size() && s[i] == s[i + offset])
-                    offset++;
-            }
-            else ++i;
-        }
-        if (s.size() > 3 && s[s.size() - 1] == s[s.size() - 2] && s[s.size() - 2] == s[s.size() - 3])
-            offset++;
-        if (offset)
-            s.resize(s.size() - offset);
-        return s;
-    } */
 };
